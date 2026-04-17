563590c556b6b497e2476e88388cfd11d5cb62d1
Nim Yanay <nimx100@gmail.com>
2026-04-17 14:50:57 -0400

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,1 @@
-import adapter from '@sveltejs/adapter-netlify';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  }
-};
-
-export default config;
+import adapter from '@sveltejs/adapter-netlify'; import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; const config = { preprocess: vitePreprocess(), kit: { adapter: adapter() } }; export default config; 
